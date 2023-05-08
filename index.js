50002c9eeb5bf922ce4155062b0e0488bea05ac4
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-05-08 21:55:28 +0300

    Link javascript files

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-import Library from "./modules/book";
+import Library from "./modules/library.js";
+import navBar from "./modules/nav.js";
+import dateNow from "./modules/date.js";
 
 
 
 
 const library = new Library();
 library.displaylist();
-library.addbtn()
+library.addbtn(library)
+navBar()
+dateNow()
